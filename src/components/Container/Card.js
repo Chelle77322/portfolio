952498bd1c5ee399f147c36aa060deac3e5b3314
Chelle77322 <serenynadarra@gmail.com>
2021-05-09 15:29:26 +0930

    Adding npm package react-bootstrap and modifying Card.js to use CardDeck

--- src/components/Container/Card.js
+++ src/components/Container/Card.js
@@ -1,23 +1,47 @@
-import PropTypes from 'prop-types';
-import Icon from './Icon' ;
+import Card from 'react-bootstrap/Card';
+import CardDeck from 'react-bootstrap/CardDeck';
 
-const Card = ({ source, alt, text}) => (
-    <div className = "card">
-        <Icon source = {source} alt = {alt}/>
-        <div className = "card-body text center">
-            <p className = "card-text">{text}</p>
-        </div>
-    </div>
 
-);
-Card.propTypes = {
-    source: PropTypes.string,
-    alt: PropTypes.string,
-    text:PropTypes.oneOfType([PropTypes.string, PropTypes.shape({}),]),
-};
-Card.defaultProps = {
-    source: "",
-    alt : "",
-    text: "",
-};
+<CardDeck>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This content is a little bit longer.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This card has supporting text below as a natural lead-in to additional
+        content.{' '}
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+  <Card>
+    <Card.Img variant="top" src="holder.js/100px160" />
+    <Card.Body>
+      <Card.Title>Card title</Card.Title>
+      <Card.Text>
+        This is a wider card with supporting text below as a natural lead-in to
+        additional content. This card has even longer content than the first to
+        show that equal height action.
+      </Card.Text>
+    </Card.Body>
+    <Card.Footer>
+      <small className="text-muted">Last updated 3 mins ago</small>
+    </Card.Footer>
+  </Card>
+</CardDeck>
 export default Card;
