c6f7aaba275dedc328fb99a9204b5d1320545deb
Chelle77322 <serenynadarra@gmail.com>
2021-05-19 13:38:06 +0930

    Removing PropTypes reference and working on overall style

---
+++ src/components/Container/Container.js
@@ -0,0 +1,13 @@
+import React,{Component} from "react";
+import Card from './Card';
+import '../Container/Container.css';
+
+export default class Container extends Component {
+    render(){
+        return (
+            <>
+            <Card />
+            </>
+        );
+    }
+}
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,18 +1,12 @@
-//import video from '../Header/logo.mp4';
-//import logo from '../Header/banner.png';
 import React, {Component} from 'react';
-
 import '../Header/Header.css';
-import '../../stylesheets/App.css';
-
-
 
 export default class Header extends Component {
     render(){
         return (
             <>
-            <div className = "App.header">
-                <h1 className = "h1">
+            <div className = "header">
+                <h1 className = "header-img">
                 
                 </h1>
             
