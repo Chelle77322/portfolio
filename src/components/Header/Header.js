e50815ad388bea3416cf3b6999aed967ff043651
Chelle77322 <serenynadarra@gmail.com>
2021-05-19 10:11:34 +0930

    Stilll working toward perfection

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,5 @@
-import video from '../Header/logo.mp4'
+import video from '../Header/logo.mp4';
+import {web_logo} from '../Header/banner.png';
 import React, {Component} from 'react';
 
 import '../Header/Header.css';
@@ -8,12 +9,14 @@ import '../Header/Header.css';
 export default class Header extends Component {
     render(){
         return (
-            
+            <>
             <div className = "header">
-                <h1 className = "Header-img"></h1>
+                <h1 className = "Header-img">
+                <img src = {web_logo} alt = "Michelle Hall Web Design"/>
+                </h1>
             
            </div>
-           
+           </>
           
         )
     }
