ce54a6b9485da39913e8efd3d15486083df68b33
Chelle77322 <serenynadarra@gmail.com>
2021-06-17 13:49:00 +0930

    Removing MERN setup with blog and chat running out of time - Moved to future developments

--- portfolio/src/components/Carousel/CarouselDot.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-export default function CarouselDot(props) {
-    const {index, currentSlideIndex, goToSlide} = props;
-    return(
-        <div key={index} onClick={()=>goToSlide(index)} className={index===currentSlideIndex ? "dotActive" : "dot"}/>
-    );
-}
--- portfolio/src/components/Carousel/CarouselPrevBtn.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-export default function CarouselPrevBtn(props) {
-    const {goToSlide, index} = props;
-    return(
-        <div className="prev" onClick={()=>goToSlide(index, true, false)}>&#10094;</div>
-    );
-}
